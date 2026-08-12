b1de045edb6cbba2470721ecba3906ced02574f7
Elmish Segara <emoukouanga@gmail.com>
2026-08-12 11:14:44 +0100

    fix: ajout de la licence1

--- next.config.js
+++ next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
       bodySizeLimit: "6mb",
     },
   },
+  // Indique à Next.js de ne pas inclure @napi-rs/canvas dans le bundle serveur
+  serverExternalPackages: ["@napi-rs/canvas"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals.push("@napi-rs/canvas");
+    }
+    return config;
+  },
 };
 
 module.exports = nextConfig;
